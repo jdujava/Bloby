c56edec1f762187ca5cf51267acd2048b7ee76f6
jdujava <jonas.dujava1@gmail.com>
2018-09-07 17:39:21 +0200

    Transfer to US

--- public/sketch.js
+++ public/sketch.js
@@ -32,7 +32,7 @@ function setup () {
 
   nameInput.changed(startOfGame)
 
-  socket = io.connect('https://tehblobs.herokuapp.com/')
+  socket = io.connect('https://bloby-game.herokuapp.com/')
   //socket = io.connect('http://localhost:5000/')
 
   socket.on('id', getId)
